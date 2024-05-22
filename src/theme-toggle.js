7f88a8e793a14bdfbd75eb672f3fc730fd2fc535
Yasin Demir <thelifeisnothing@gmail.com>
2024-05-22 15:42:05 +0200

    Update theme-toggle.js

--- src/theme-toggle.js
+++ src/theme-toggle.js
@@ -1,58 +1,32 @@
 $(document).ready(function () {
-    // ThemeManager class to handle theme switching functionality
     class ThemeManager {
         constructor() {
             this.themeSwitch = $('#theme-switch');
             this.currentTheme = localStorage.getItem('theme') || 'light';
+            this.body = $('body');
             this.init();
         }
 
-        // Initialize theme manager
         init() {
             this.applyCurrentTheme();
             this.themeSwitch.prop('checked', this.currentTheme === 'dark');
             this.themeSwitch.on('change', () => this.toggleTheme());
         }
 
-        // Apply the current theme to the body
         applyCurrentTheme() {
-            $('body').addClass(this.currentTheme);
+            this.body.addClass(this.currentTheme);
         }
 
-        // Toggle between light and dark themes
         toggleTheme() {
             if (this.themeSwitch.is(':checked')) {
-                $('body').removeClass('light').addClass('dark');
+                this.body.removeClass('light').addClass('dark');
                 localStorage.setItem('theme', 'dark');
             } else {
-                $('body').removeClass('dark').addClass('light');
+                this.body.removeClass('dark').addClass('light');
                 localStorage.setItem('theme', 'light');
             }
         }
     }
 
-    // SidebarManager class to handle the README sidebar
-    class SidebarManager {
-        constructor() {
-            this.readmeButton = $('#readme-button');
-            this.readmeSidebar = $('#readme-sidebar');
-            this.init();
-        }
-
-        // Initialize sidebar manager
-        init() {
-            this.readmeButton.on('click', () => this.toggleSidebar());
-        }
-
-        // Toggle the sidebar visibility
-        toggleSidebar() {
-            this.readmeSidebar.toggleClass('active');
-        }
-    }
-
-
-    // Instantiate the ThemeManager class
     new ThemeManager();
-    new SidebarManager();
-
 });
